daae777b4c2c4fd0cebebfa91915601eda00d32b
vanshika <vanshikav.kumar@gmail.com>
2022-01-18 21:37:01 +0530

    First Push Test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # StudentHelpDesk
 An Android App, The app is aimed to help the placement cell and students to review and change the data in real time.
+Often it happens that placement cell urgently needs data of students but students are not availaible at the moment.
+Or for student to change an information as tangible as their phone number, have to write mails to the placement cells.
+Placement cell sends long form every few months to students for their updated information which is a tedious task.
+Our app solved all these problems by providing a single platform to both students and admins to view review and change data.
--- app/src/main/java/com/example/studenthelpdesk/RegisterCollege.java
+++ app/src/main/java/com/example/studenthelpdesk/RegisterCollege.java
@@ -3,12 +3,35 @@ package com.example.studenthelpdesk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegisterCollege extends AppCompatActivity {
-
+    //this instance will be referred to untill registration is completed
+    static CollegeRegistrationData allData;
+    EditText uname,cName,password,password2,adminmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_college);
+        allData=new CollegeRegistrationData();
+        uname=findViewById(R.id.uname);
+        cName=findViewById(R.id.cname);
+        adminmail=findViewById(R.id.adminemail);
+        password=findViewById(R.id.password2);
+        password2=findViewById(R.id.confirmpassword2);
+
+    }
+    //move to next step of registration
+    public void next(View v)
+    {
+        checkConstraints();
+    }
+    public Boolean checkConstraints()
+    {
+        //check if valid mail
+        //check if passwords match
+        //check if
+        return true;
     }
 }
