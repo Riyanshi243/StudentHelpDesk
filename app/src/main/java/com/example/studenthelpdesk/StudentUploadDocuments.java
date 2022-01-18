d70d84a96ff8d6680c2e90df72e1e8f04ceeae3d
Riyanshi243 <riyanshiverma243@gmail.com>
2022-01-18 22:59:44 +0530

    Student section xmls done

---
+++ app/src/main/java/com/example/studenthelpdesk/StudentUploadDocuments.java
@@ -0,0 +1,14 @@
+package com.example.studenthelpdesk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentUploadDocuments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_upload_documents);
+    }
+}
