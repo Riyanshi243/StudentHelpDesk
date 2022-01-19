e79d129410d6361e0ad931a2274b76fd8828e7a3
vanshika <vanshikav.kumar@gmail.com>
2022-01-19 14:40:14 +0530

    Register till step 3 done

--- app/src/main/java/com/example/studenthelpdesk/CollegeRegistrationData.java
+++ app/src/main/java/com/example/studenthelpdesk/CollegeRegistrationData.java
@@ -1,8 +1,41 @@
 package com.example.studenthelpdesk;
+
+import java.util.ArrayList;
+
 //at the time of registration all the details of the college will be stored as an object of this class
 public class CollegeRegistrationData {
-    private String Uname,SAdminemail,password,CName;
+    private String Uname,SAdminemail,password,CName,Location;
+    private ArrayList<String> deptName,courseName;
+    private ArrayList<ArrayList<String>> branchForEachCourse;
 
+    public void setBranchForEachCourse(ArrayList<ArrayList<String>> branchForEachCourse) {
+        this.branchForEachCourse = branchForEachCourse;
+    }
+
+    public void setCourseName(ArrayList<String> courseName) {
+        this.courseName = courseName;
+    }
+
+    public ArrayList<ArrayList<String>> getBranchForEachCourse() {
+        return branchForEachCourse;
+    }
+
+    public ArrayList<String> getCourseName() {
+        return courseName;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+    public ArrayList<String> getDeptName() {
+        return deptName;
+    }
+    public String getLocation() {
+        return Location;
+    }
+    public void setDeptName(ArrayList<String> deptName) {
+        this.deptName = deptName;
+    }
     public String getUname() {
         return Uname;
     }
--- app/src/main/java/com/example/studenthelpdesk/RegisterCollege.java
+++ app/src/main/java/com/example/studenthelpdesk/RegisterCollege.java
@@ -2,14 +2,21 @@ package com.example.studenthelpdesk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegisterCollege extends AppCompatActivity {
     //this instance will be referred to untill registration is completed
     static CollegeRegistrationData allData;
     EditText uname,cName,password,password2,adminmail;
+    FirebaseFirestore firebaseFirestores;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,18 +27,44 @@ public class RegisterCollege extends AppCompatActivity {
         adminmail=findViewById(R.id.adminemail);
         password=findViewById(R.id.password2);
         password2=findViewById(R.id.confirmpassword2);
+        firebaseFirestores=FirebaseFirestore.getInstance();
 
     }
     //move to next step of registration
     public void next(View v)
     {
-        checkConstraints();
+        if(checkConstraints())
+        {
+            //intent to college registration step 2
+            startActivity(new Intent(RegisterCollege.this, RegisterCollege2.class));
+            finish();
+        }
     }
+    public void toLogin(View v)
+    {
+        //Intent to login
+    }
+
     public Boolean checkConstraints()
     {
+        final Boolean[] flag = {true};
+        //everything not empty
         //check if valid mail
         //check if passwords match
-        //check if
-        return true;
+        //check if password length greater than 8
+        //check if username already exists
+        DocumentReference documentReference = firebaseFirestores.collection("CollegeData").document(uname.getText().toString());
+        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                //if username exist... a document will already be present in its name
+                if(documentSnapshot.exists())
+                {
+                    uname.setError("This username already Exist");
+                    flag[0] =false;
+                }
+            }
+        });
+        return flag[0];
     }
 }
