9523052f699d022d86f82251d96542c49eaa87ab
Riyanshi243 <riyanshiverma243@gmail.com>
2022-01-19 17:42:58 +0530

    admin and college sign up

--- app/src/main/java/com/example/studenthelpdesk/AdminCreateNewAccount.java
+++ app/src/main/java/com/example/studenthelpdesk/AdminCreateNewAccount.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Signup extends AppCompatActivity {
+public class AdminCreateNewAccount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_admin_create_new_account);
     }
 }
---
+++ app/src/main/java/com/example/studenthelpdesk/AdminSignUp.java
@@ -0,0 +1,14 @@
+package com.example.studenthelpdesk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_sign_up);
+    }
+}
