b446a18464d7a32265f6504849c1634d7a17ac7d
Riyanshi243 <riyanshiverma243@gmail.com>
2022-02-21 23:16:32 +0530

    notification received layouts fixed!

---
+++ app/src/main/java/com/example/studenthelpdesk/ViewNotificationDataByAll.java
@@ -0,0 +1,14 @@
+package com.example.studenthelpdesk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewNotificationDataByAll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_notification_data_by_all);
+    }
+}
