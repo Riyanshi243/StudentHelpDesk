3f1a56a16860d26179058be7248ca1a9390cbfd0
HP <auditorvkumar@gmail.com>
2022-03-19 23:58:29 +0530

    headings in view all

--- app/src/main/java/com/example/studenthelpdesk/AdminViewAllStudentData.java
+++ app/src/main/java/com/example/studenthelpdesk/AdminViewAllStudentData.java
@@ -7,25 +7,151 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class AdminViewAllStudentData extends AppCompatActivity {
 
     CollegeRegisterQuestions allpeople;
-
+    ArrayList<CollegeRegisterQuestions> personalQ,academicQ,uploadQ;
     AutoCompleteTextView sortin;
-
+    AdminData adminData;
+    TableLayout tl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_view_all_student_data);
         sortin=findViewById(R.id.sortin);
+        adminData=AdminPage.adminData;
+        tl=findViewById(R.id.TableLayout);
+        personalQ=new ArrayList<>();
+        academicQ=new ArrayList<>();
+        uploadQ=new ArrayList<>();
         String[] sortinList={"Ascending Order","Descending Order"}; //Ascending = 0 and descending =1
         ArrayAdapter spinnerList=new ArrayAdapter(this,android.R.layout.simple_spinner_item,sortinList);
         sortin.setAdapter(spinnerList);
+        DocumentReference personalDetails= FirebaseFirestore.getInstance().collection("All Colleges").document(adminData.getCollegeId()).collection("Questions").document("Personal Question");
+        personalDetails.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                int total=(int)documentSnapshot.get("Total");
+                for (int i=0;i<total;i++)
+                {
+                    int finalI = i;
+                    int finalI1 = i;
+                    personalDetails.collection(i+"").document(i+"")
+                            .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            CollegeRegisterQuestions crq=new CollegeRegisterQuestions();
+                            crq.setQuestion((String) documentSnapshot.get("Question"));
+                            crq.setType((int) documentSnapshot.get("Type"));
+                            crq.setId(finalI);
+                            personalQ.add(crq);
+                            if(finalI1 ==total-1)
+                            {
+                               DocumentReference academicDetails=FirebaseFirestore.getInstance().collection("All Colleges").document(adminData.getCollegeId()).collection("Questions").document("Academic Question");
+                               academicDetails.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                   @Override
+                                   public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                     int total= (int) documentSnapshot.get("Total");
+                                     for (int i=0;i<total;i++)
+                                     {
+                                         int finalI2 = i;
+                                         academicDetails.collection(i+"").document(i+"")
+                                                 .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                             @Override
+                                             public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                                 CollegeRegisterQuestions crq=new CollegeRegisterQuestions();
+                                                 crq.setQuestion((String) documentSnapshot.get("Question"));
+                                                 crq.setType((int) documentSnapshot.get("Type"));
+                                                 crq.setId(finalI2);
+                                                 academicQ.add(crq);
+                                                 if(finalI2 ==total-1)
+                                                 {
+                                                    DocumentReference uploadDetails=FirebaseFirestore.getInstance().collection("All Colleges").document(adminData.getCollegeId()).collection("Questions").document("Upload Question");
+                                                    uploadDetails.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                                        @Override
+                                                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                                            int total=(int)documentSnapshot.get("Total");
+                                                            for(int i=0;i<total;i++)
+                                                            {
+                                                                int finalI3 = i;
+                                                                uploadDetails.collection(i+"").document(i+"")
+                                                                        .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                                                    @Override
+                                                                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                                                        CollegeRegisterQuestions crq=new CollegeRegisterQuestions();
+                                                                        crq.setQuestion((String) documentSnapshot.get("Question"));
+                                                                        crq.setType((int) documentSnapshot.get("Type"));
+                                                                        crq.setId(finalI3);
+                                                                        uploadQ.add(crq);
+                                                                        show();
+                                                                    }
+                                                                });
 
+                                                            }
+
+                                                        }
+                                                    });
+                                                 }
+
+                                             }
+                                         });
+
+                                     }
+                                   }
+                               });
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
     public void filters(View v){
         startActivity(new Intent(AdminViewAllStudentData.this, AdminViewAllStudentDataFilters.class));
+    }
+    ArrayList<StudentData> studentData=new ArrayList<>();
+    public void get()
+    {
+
+    }
+    public void show()
+    {
+        //heading
+        TableRow heading=new TableRow(this);
+        for(int i=0;i<personalQ.size();i++)
+        {
+            View headingname=getLayoutInflater().inflate(R.layout.repeatable_table_header,null);
+            TextView name=headingname.findViewById(R.id.table_head);
+            name.setText((CharSequence) personalQ.get(i));
+            heading.addView(headingname);
+        }
+        for(int i=0;i<academicQ.size();i++)
+        {
+            View headingname=getLayoutInflater().inflate(R.layout.repeatable_table_header,null);
+            TextView name=headingname.findViewById(R.id.table_head);
+            name.setText((CharSequence) academicQ.get(i));
+            heading.addView(headingname);
+        }
+        for(int i=0;i<uploadQ.size();i++)
+        {
+            View headingname=getLayoutInflater().inflate(R.layout.repeatable_table_header,null);
+            TextView name=headingname.findViewById(R.id.table_head);
+            name.setText((CharSequence) uploadQ.get(i));
+            heading.addView(headingname);
+        }
+        tl.addView(heading);
+
     }
 
 }
