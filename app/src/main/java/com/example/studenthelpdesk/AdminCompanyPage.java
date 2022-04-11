270cb7ac43b86678b6665e914d23d28f879f35ee
Riyanshi243 <riyanshiverma243@gmail.com>
2022-04-11 11:40:00 +0530

    layouts corrected

--- app/src/main/java/com/example/studenthelpdesk/AdminCompanyPage.java
+++ app/src/main/java/com/example/studenthelpdesk/AdminCompanyPage.java
@@ -31,22 +31,12 @@ public class AdminCompanyPage extends AppCompatActivity {
         setContentView(R.layout.activity_admin_company_page);
         name=findViewById(R.id.name);
         profilepic=findViewById(R.id.profilePic);
-        adminData=AdminPage.adminData;
 
-        name.setText(adminData.getAdminName());
-        StorageReference storageReference = FirebaseStorage.getInstance().getReference(adminData.getCollegeId()).child("Photograph").child(adminData.getEmail());
-        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                Glide.with(AdminCompanyPage.this)
-                        .load(uri).diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .error(R.drawable.admin_profile_img)
-                        .placeholder(R.drawable.default_loading_img)
-                        .into(profilepic);
-            }
-        });
     }
     public void viewListOfCompanies(View v){
         startActivity(new Intent(AdminCompanyPage.this, AdminViewListOfCompanies.class));
+    }
+    public void viewDataRequestsFromCompany(View v){
+        startActivity(new Intent(AdminCompanyPage.this, AdminViewDataRequestsFromCompany.class));
     }
 }
