1ee8495c47febf4570d5c7e25ff75fd4bd4631d2
Riyanshi243 <riyanshiverma243@gmail.com>
2022-04-12 15:55:22 +0530

    Data requests layouts

---
+++ app/src/main/java/com/example/studenthelpdesk/CompanyViewStudentsData.java
@@ -0,0 +1,14 @@
+package com.example.studenthelpdesk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompanyViewStudentsData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_view_students_data);
+    }
+}
